38ef6b09b679451c13191e1b947a8451ed2bc2a3
Sagar Jha <99650187+Sagarjha11@users.noreply.github.com>
2026-01-22 20:21:45 +0530

    Add files via upload

---
+++ Assignment 1/Task1.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+add = num1 + num2
+sub = num1 - num2
+multiply = num1 * num2
+divide = num1 / num2
+print("Addition of ",num1, "and",num2,":",add)
+print("Subtraction of ",num1, "and",num2,":",sub)
+print("Multiplication of ",num1, "and",num2,":",multiply)
+print("Division of ",num1, "and",num2,":",divide)
---
+++ Assignment 1/Task2.py
@@ -0,0 +1,3 @@
+first = input("Eneter your first name : " )
+last = input("enter your last name : ")
+print(f"Hello, {first + last}! Welcome to the Pthon program")
